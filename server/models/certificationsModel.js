cf5e8698405aa83e75d8680fd962e3f111614449
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-06-02 11:21:15 +0200

    Added a sort middleware to the certifications model

--- server/models/certificationsModel.js
+++ server/models/certificationsModel.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Define the schema for the covered text within a certification
 const CertificationCoveredSchema = new mongoose.Schema({
   text: {
     type: String,
-    required: [true, "Covered text must be present."],
+    required: [true, 'Covered text must be present.'],
   },
 });
 
 // Exclude '__v' field from query results
 CertificationCoveredSchema.pre(/^find/, function (next) {
-  this.select("-__v");
+  this.select('-__v');
   next();
 });
 
@@ -18,29 +18,34 @@ CertificationCoveredSchema.pre(/^find/, function (next) {
 const CertificationsSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "A certification must have a title"],
-    unique: [true, "This title is already in the database"],
+    required: [true, 'A certification must have a title'],
+    unique: [true, 'This title is already in the database'],
   },
   instructor: {
     type: String,
-    required: [true, "A certification must have a teacher or instructor."],
+    required: [true, 'A certification must have a teacher or instructor.'],
   },
   photo: {
     type: String,
-    required: [true, "A certification must have an image."],
+    required: [true, 'A certification must have an image.'],
   },
   organization: {
     type: String,
-    required: [true, "A certification must have an organization. "],
+    required: [true, 'A certification must have an organization. '],
   },
   covered: [CertificationCoveredSchema],
+  date: {
+    type: Date,
+    default: new Date().toUTCString(),
+  },
 });
 
 // Exclude '__v' field from query results
 CertificationsSchema.pre(/^find/, function (next) {
-  this.select("-__v");
+  this.select('-__v');
+  this.sort({ date: 1 });
   next();
 });
 
 // Create and export the Certification model
-module.exports = mongoose.model("certification", CertificationsSchema);
+module.exports = mongoose.model('certification', CertificationsSchema);
