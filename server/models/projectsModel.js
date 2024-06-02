71a6d1c4ac83102a8a980fa49791a26a641bc7b2
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-06-02 11:31:19 +0200

    Added a sort middleware to the certifications model

--- server/models/projectsModel.js
+++ server/models/projectsModel.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Define the schema for project images
 const ProjectImagesSchema = new mongoose.Schema({
   original: {
     type: String,
-    required: [true, "Original image is necessary."],
+    required: [true, 'Original image is necessary.'],
   },
   thumbnail: {
     type: String,
-    required: [true, "Thumbnail image is necessary."],
+    required: [true, 'Thumbnail image is necessary.'],
   },
   originalAlt: {
     type: String,
-    required: [true, "Image description is necessary"],
+    required: [true, 'Image description is necessary'],
   },
   originalTitle: {
     type: String,
-    required: [true, "Original image title is necessary."],
+    required: [true, 'Original image title is necessary.'],
   },
 });
 
@@ -24,8 +24,8 @@ const ProjectImagesSchema = new mongoose.Schema({
 const ProjectSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "A project must have a title"],
-    unique: [true, "This title is already in the database."],
+    required: [true, 'A project must have a title'],
+    unique: [true, 'This title is already in the database.'],
   },
   current: {
     type: Boolean,
@@ -33,7 +33,7 @@ const ProjectSchema = new mongoose.Schema({
   },
   description: {
     type: String,
-    required: [true, "A project must have a description"],
+    required: [true, 'A project must have a description'],
   },
 
   demo: {
@@ -42,7 +42,7 @@ const ProjectSchema = new mongoose.Schema({
 
   code: {
     type: String,
-    required: [true, "A project must have a source code"],
+    required: [true, 'A project must have a source code'],
   },
 
   images: [ProjectImagesSchema],
@@ -57,10 +57,10 @@ ProjectSchema.index({ title: 1 });
 
 // Exclude '__v' field from query results, sort results by 'created_at' field in descending order
 ProjectSchema.pre(/^find/, function (next) {
-  this.select(["-__v"]);
+  this.select(['-__v']);
   this.sort({ created_at: -1 });
   next();
 });
 
 // Create and export the Project model
-module.exports = mongoose.model("project", ProjectSchema);
+module.exports = mongoose.model('project', ProjectSchema);
